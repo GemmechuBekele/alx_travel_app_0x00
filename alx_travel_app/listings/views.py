67dc17ac0e3a313c2a72a9bc5bff8c20dd1f309e
GemmechuBekele <gemmechu.bekele.berga@gmail.com>
2025-06-01 15:25:08 +0300

    Modify Database models, create serializers for API data representation, and implement a management command to seed the database.

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+
+class ListingListCreateView(generics.ListCreateAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    filterset_fields = ['location', 'price', 'amenities']
+    search_fields = ['title', 'description']
+
+class BookingListCreateView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    filterset_fields = ['listing', 'user', 'start_date', 'end_date']
+    search_fields = ['user__username']
